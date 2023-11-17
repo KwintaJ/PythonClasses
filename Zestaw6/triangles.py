1815c9b9d97fd92f97a50e2c1beee8f9dd071e2e
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2023-11-17 16:16:21 +0100

    Add files via upload

---
+++ Zestaw6/triangles.py
@@ -0,0 +1,54 @@
+#####################################
+#                                   #
+#  Zestaw 6             21.11.2023  #
+#                                   #
+#           Zadanie 6.4             #
+#     Plik z modulem triangles      #
+#                                   #
+#  Jan Kwinta                       #
+#                                   #
+#####################################
+
+from points import Point
+from math import sqrt as Msqrt
+
+class Triangle:
+
+    def __init__(self, x1, y1, x2, y2, x3, y3):  # konstuktor
+        self.pt1 = Point(x1, y1)
+        self.pt2 = Point(x2, y2)
+        self.pt3 = Point(x3, y3)
+
+    def __str__(self):
+        return "[" + str(self.pt1) + ", " + str(self.pt2) + ", " + str(self.pt3) + "]"
+
+    def __repr__(self):
+        return "Triangle(" + str(self.pt1.x) + ", " + str(self.pt1.y) + ", " + str(self.pt2.x) + ", " + str(self.pt2.y) + ", " + str(self.pt3.x) + ", " + str(self.pt3.y) + ")"
+
+    def __eq__(self, other):
+        if self.pt1 != other.pt1 and self.pt1 != other.pt2 and self.pt1 != other.pt3:
+            return False
+        if self.pt2 != other.pt1 and self.pt2 != other.pt2 and self.pt2 != other.pt3:
+            return False
+        if self.pt3 != other.pt1 and self.pt3 != other.pt2 and self.pt3 != other.pt3:
+            return False
+        return True
+        
+
+    def __ne__(self, other):
+        return not self == other
+
+    def center(self):
+        S = self.pt1 + self.pt2 + self.pt3
+        return Point(round(S.x / 3, 8), round(S.y / 3, 8))
+
+    def area(self):
+        a = (self.pt2 - self.pt1).length()
+        b = (self.pt3 - self.pt2).length()
+        c = (self.pt1 - self.pt3).length()
+        return round((Msqrt((a + b + c) * (b + c - a) * (a - b + c) * (a + b - c))) / 4, 8)
+
+    def move(self, x, y):
+        M = Point(x, y)
+        return Triangle(self.pt1 + M, self.pt2 + M, self.pt3 + M)
+
