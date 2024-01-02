90d54fe1b821e921e7faddfc3f0a63685872d656
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-02 13:56:00 +0100

    Update vectors.py

---
+++ Projekt/vectors.py
@@ -0,0 +1,23 @@
+######################################
+#  Jan Kwinta            02.01.2024  #
+#                                    #
+#        Projekt zaliczeniowy        #
+#       Plik z modulem Vector        #
+#                                    #
+######################################
+
+from points import Point
+
+class Vector:
+    def __init__(self, A, B):
+        self.pointA = A
+        self.pointB = B
+
+    def __str__(self):
+        return "[" + str(self.pointA) + ", " + str(self.pointB) + "]"
+
+    def __eq__(self, other):
+        return self.pointA == other.pointA and self.pointB == other.pointB
+
+    def __ne__(self, other):
+        return not self == other
