200cb0f77b49c1e1fc5ff48a8b1a7f3ee2f9625b
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-02 13:56:18 +0100

    Update points.py

--- Projekt/points.py
+++ Projekt/points.py
@@ -7,15 +7,12 @@
 ######################################
 
 class Point:
-    def __init__(self, x, y):  # konstuktor
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
-
-    def __repr__(self):
-        return "Point(" + str(self.x) + ", " + str(self.y) + ")"
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
