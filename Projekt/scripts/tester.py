1c97f30f73d835848bf23701cbc46e0ea1287d26
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-02 14:37:38 +0100

    Create tester.py

---
+++ Projekt/scripts/tester.py
@@ -0,0 +1,26 @@
+######################################
+#  Jan Kwinta            02.01.2024  #
+#                                    #
+#        Projekt zaliczeniowy        #
+#    Plik do testowania ConvexHull   #
+#                                    #
+######################################
+
+# !/usr/bin/env/python
+# coding = utf-8
+from points import Point
+from vectors import Vector
+from HullModule import ConvexHull
+import unittest
+
+class TestConvexHull(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+    def tearDown(self): pass
+
+if __name__ == '__main__':
+    unittest.main()
