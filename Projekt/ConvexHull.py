cdfb50e01f159e4e4449bfbc0edf481370d0c943
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-02 13:59:25 +0100

    Create ConvexHull.py

---
+++ Projekt/ConvexHull.py
@@ -0,0 +1,11 @@
+######################################
+#  Jan Kwinta            02.01.2024  #
+#                                    #
+#        Projekt zaliczeniowy        #
+#             Plik glowny            #
+#                                    #
+######################################
+
+from points import Point
+from vectors import Vector
+
