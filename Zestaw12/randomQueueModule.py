1d6d08dd362782c15def634debd87effb9e90981
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-16 10:34:57 +0100

    Update randomQueueModule.py

---
+++ Zestaw12/randomQueueModule.py
@@ -0,0 +1,13 @@
+class RandomQueue:
+    def __init__(self): pass
+    #def __init__(self, size=10): pass   # wersja z ograniczeniem na rozmiar
+
+    def insert(self, item): pass   # wstawia element w czasie O(1)
+
+    def remove(self): pass   # zwraca losowy element w czasie O(1)
+
+    def is_empty(self): pass
+
+    def is_full(self): pass
+
+    def clear(self): pass   # czyszczenie listy
