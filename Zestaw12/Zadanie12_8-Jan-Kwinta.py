d084defef84a7bc3d503ba716304372ebcb56933
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-16 10:50:22 +0100

    Update Zadanie12_8-Jan-Kwinta.py

--- Zestaw12/Zadanie12_8-Jan-Kwinta.py
+++ Zestaw12/Zadanie12_8-Jan-Kwinta.py
@@ -1,8 +1,20 @@
+#####################################
+#                                   #
+#  Zestaw 12            16.01.2024  #
+#                                   #
+#           Zadanie 12.8            #
+#   Plik z testujacy implementacje  #
+#          kolejki losowej          #
+#                                   #
+#  Jan Kwinta                       #
+#                                   #
+#####################################
+
 import unittest
 from randomQueueModule import RandomQueue
 
 class TestRandomQueue(unittest.TestCase):
-    def test_insert_and_remove(self):
+    def testInsertAndERmove(self):
         q = RandomQueue()
         q.insert(1)
         self.assertFalse(q.is_empty())
@@ -10,19 +22,19 @@ class TestRandomQueue(unittest.TestCase):
         self.assertEqual(item, 1)
         self.assertTrue(q.is_empty())
 
-    def test_insert_full_queue(self):
+    def testInsertIntoFullQueue(self):
         q = RandomQueue(size=2)
         q.insert(1)
         q.insert(2)
         with self.assertRaises(ValueError):
             q.insert(3)
 
-    def test_remove_empty_queue(self):
+    def testRemoveEmptyQueue(self):
         q = RandomQueue()
         with self.assertRaises(ValueError):
             q.remove()
 
-    def test_clear(self):
+    def testClear(self):
         q = RandomQueue()
         q.insert(1)
         q.insert(2)
