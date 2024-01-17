cd1f4879fb7e02ce247e32a736c863e5984f9cb6
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-17 14:12:26 +0100

    Update Zadanie12_2-Jan-Kwinta.py

---
+++ Zestaw12/Zadanie12_2-Jan-Kwinta.py
@@ -0,0 +1,12 @@
+#####################################
+#                                   #
+#  Zestaw 12            16.01.2024  #
+#                                   #
+#           Zadanie 12.2            #
+#   Plik testujacy implementacje    #
+#          tablicowa stosu          #
+#                                   #
+#  Jan Kwinta                       #
+#                                   #
+#####################################
+
