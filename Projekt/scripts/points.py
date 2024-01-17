10c452981996dd1d3fe48403aaf80d2d7f439d1d
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-17 22:39:07 +0100

    Add < operator to class Point

--- Projekt/scripts/points.py
+++ Projekt/scripts/points.py
@@ -19,3 +19,8 @@ class Point:
 
     def __ne__(self, other):
         return not self == other
+        
+    def __lt__(self, other):
+        if self.x != other.x:
+            return self.x < other.x
+        return self.y < other.y
