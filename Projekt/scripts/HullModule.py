c65adeaac8cd13409649de7572c46a8f2ee4de77
Jan Kwinta <94843940+KwintaJ@users.noreply.github.com>
2024-01-17 22:05:47 +0100

    Update HullModule.py

--- Projekt/scripts/HullModule.py
+++ Projekt/scripts/HullModule.py
@@ -6,10 +6,31 @@
 #                                    #
 ######################################
 
+from math import atan2
 from points import Point
 from vectors import Vector
 
 class ConvexHull:
     def __init__(self, L):
         self.pointsList = L
-        self.hullList = []
+        self.hullList = self.computeHull()
+        
+    def computeHull(self):
+        if len(self.pointsList) < 3:
+            return self.pointsList  
+        
+        pivot = min(self.pointsList, key=lambda p: (p.y, p.x))
+        sorted_points = sorted(self.pointsList, key=lambda p: (atan2(p.y - pivot.y, p.x - pivot.x), p))
+
+        hull = [pivot, sorted_points[0], sorted_points[1]]
+
+        for point in sorted_points[2:]:
+            while len(hull) > 1 and (Vector(hull[-2], hull[-1]).cross(Vector(hull[-2], point)) <= 0):
+                hull.pop()
+            hull.append(point)
+
+        return hull
+    
+    def getHull(self):
+        return self.hullList
+
